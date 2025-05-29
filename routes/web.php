6d7693411628f3a6226190bfbd3f3e9e276a1044
AlulCode45 <alulgans70@gmail.com>
2025-05-30 01:03:54 +0700

    Add broadcasting channels for user and chat access; update web routes for chat functionality

--- routes/web.php
+++ routes/web.php
@@ -5,7 +5,12 @@ use App\Http\Controllers\ChatController;
 use Illuminate\Support\Facades\Route;
 
 
-Route::get('/', [AuthController::class, 'loginPage'])->name('home');
+Route::get('/', [AuthController::class, 'loginPage'])->name('login');
 Route::post('', [AuthController::class, 'loginProcess'])->name('loginProcess');
 
-Route::resource('/chats', ChatController::class)->middleware('auth');
+
+Route::middleware(['auth'])->group(function () {
+    Route::get('/chat/{user}', [ChatController::class, 'viewChat'])->name('chat.view');
+    Route::get('/chats', [ChatController::class, 'index']);
+    Route::post('/chats', [ChatController::class, 'store']);
+});
