e11f772ee83faf99385b91a811e02d50d44326b8
AlulCode45 <alulgans70@gmail.com>
2025-05-30 01:04:29 +0700

    Integrate Laravel Echo and Pusher for real-time event broadcasting; update bootstrap.js and create echo.js for configuration

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -1,4 +1,23 @@
 import axios from 'axios';
-window.axios = axios;
+import Echo from 'laravel-echo';
+import Pusher from 'pusher-js';
 
+
+window.axios = axios;
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+window.Pusher = Pusher;
+
+window.Echo = new Echo({
+    broadcaster: 'pusher',
+    key: import.meta.env.VITE_PUSHER_APP_KEY,
+    cluster: import.meta.env.VITE_PUSHER_APP_CLUSTER,
+    forceTLS: true,
+    authEndpoint: "/broadcasting/auth", // penting untuk private channel
+});
+
+window.Echo.connector.pusher.connection.bind('connected', () => {
+    console.log('✅ Pusher Connected!');
+});
+window.Echo.connector.pusher.connection.bind('error', (err) => {
+    console.error('Pusher Error:', err);
+});
